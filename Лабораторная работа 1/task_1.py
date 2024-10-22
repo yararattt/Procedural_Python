647d7b553516964e560d54eec96a249a3e0289ec
yararattt <116680653+yararattt@users.noreply.github.com>
2024-10-22 23:30:22 +0300

    Update task_1.py

--- Лабораторная работа 1/task_1.py
+++ Лабораторная работа 1/task_1.py
@@ -1,7 +1,10 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
-sum_num = sum(numbers[:4] + numbers[5:])
+index = 4
+sum_before = sum(numbers[:index])
+sum_after = sum(numbers[index + 1:])
 cnt = len(numbers)
-average = round(sum_num / cnt, 2)
-numbers[4] = average
+sum = sum_before + sum_after
+average = round(sum / cnt, 2)
+numbers[index] = average
 print("Измененный список:", numbers)
