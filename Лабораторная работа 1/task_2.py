9c50bcae534a77bf78c0277645afac4d1c71ac00
yararattt <116680653+yararattt@users.noreply.github.com>
2024-10-22 23:30:58 +0300

    Update task_2.py

--- Лабораторная работа 1/task_2.py
+++ Лабораторная работа 1/task_2.py
@@ -1,6 +1,4 @@
-import math
-
 disketa_byt = 1.44 * 1024 * 1024
 mem_book = 4 * 25 * 50 * 100
-cnt = math.floor(disketa_byt / mem_book)
+cnt = int(disketa_byt / mem_book)
 print("Количество книг, помещающихся на дискету:", cnt)
